6e7cfb7dd12d13003d8b36cb59bf7b703d43e27f
Milo Rastgoo <msrastgoo@gmail.com>
2020-06-07 16:25:44 -0400

    ex2 completed

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,15 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    path = {}
+    for ticket in tickets:
+        path[ticket.source] = ticket.destination
+    print(path)
 
-    return route
+    res = []
+    res.append(path['NONE'])
+
+    for i in range(len(path) - 1):
+        res.append(path[res[i]])
+
+    return res
