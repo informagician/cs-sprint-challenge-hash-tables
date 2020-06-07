76e0f5b2aa57a7620c9df7759520f37294e37e42
Milo Rastgoo <msrastgoo@gmail.com>
2020-06-07 17:27:59 -0400

    stretch completed

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,20 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    # to power of two
+
+    hash = {}
+
+    for item in a:
+        if item**2 in hash:
+            hash[item**2] += 1
+        else:
+            hash[item**2] = 1
+    
+    result = []
+    for item in hash:
+        if hash[item] == 2:
+            result.append(math.sqrt(item))
 
     return result
 
